721eec0c25d5d5deb04be065f36f4caeddaeb302
mou <xiaoximou@gmail.com>
2017-04-28 11:41:58 +0800

    add host edit

--- devops/asset/urls.py
+++ devops/asset/urls.py
@@ -4,4 +4,5 @@ import views
 
 urlpatterns = [
     url(r'^host_list', views.host_list, name='host_list'),
+    url(r'^host_edit', views.host_edit, name='host_edit'),
 ]
--- devops/asset/views.py
+++ devops/asset/views.py
@@ -11,3 +11,23 @@ def host_list(request):
     hosts = asset.host_list()
 
     return render(request, 'asset/host_list.html', {'hosts' : hosts})
+
+def host_edit(request):
+
+    asset = Asset()
+    if request.method == 'GET':
+
+        private_ip = request.GET.get('private_ip')
+
+        host = asset.host_query(private_ip)
+
+        return render(request, 'asset/host_edit.html', {'host' : host})
+
+    private_ip = request.POST.get('private_ip')
+    status     = request.POST.get('status')
+
+    asset.host_update_status(private_ip, status)
+
+    host = asset.host_query(private_ip)
+
+    return render(request, 'asset/host_edit.html', {'host' : host})
